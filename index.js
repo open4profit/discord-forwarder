db94829e4e577838a4e677713ae5f82c693166d0
ZIAUL HAQUE <56592240+open4profit@users.noreply.github.com>
2020-06-29 10:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,9 +36,10 @@ client.on('message', message => {
     const channel = client.channels.get(message.channel.id)
     let content = `**${message.channel.parent.name}**\n${channel.name}\n${message.content}\n`   
     message.attachments.forEach(attachment => {      
-    content += ` ${attachment.ProxyUrl} \n`    
+    content += ` ${attachment.ProxyUrl} \n` 
+    content = content.replace(/\[1\]/g, '');
   })
-    content = content.replace(/\[1\]/g, '');    
+        
     config.WRITING_CHANNELS.forEach(channel => {
       client.channels.get(channel).send(content, {embed: message.embeds[0]}).catch(err => {
         console.error(err);
